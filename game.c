3f0e6ebe0467c4f37a1b48964cc785747630245f
xplusc <keenannewcomer@gmail.com>
2024-07-03 20:32:14 -0700

    gameRender(): Implemented drawing from a list of SDL_Rects

--- game.c
+++ game.c
@@ -5,8 +5,9 @@
 #include <SDL_image.h>
 
 const Uint64 MS_PER_UPDATE = 16;
+const size_t RECTS_SIZE = 10;
 
-int gameInitialize(SDL_Renderer *rend, SDL_Texture **tex_bubble)
+int gameInitialize(SDL_Renderer *rend, SDL_Texture **tex_bubble, SDL_Rect *rects, const size_t num_rects)
 {
 	// load assets
 	*tex_bubble = IMG_LoadTexture(rend, "bubble.png");
@@ -14,21 +15,33 @@ int gameInitialize(SDL_Renderer *rend, SDL_Texture **tex_bubble)
 		fprintf(stderr, "Error creating texture: %s\n", IMG_GetError());
 		return -1;
 	}
+	
+	for (size_t i = 0; i < num_rects; ++i) {
+		SDL_Rect r = {640 / 2 - 50 / 2 + 60 * ((int) i - 5), 480 / 2 - 50 / 2, 50, 50};
+		rects[i] = r;
+	}
+	
 	return 0;
 }
 
-void gameUpdate(Uint64 current_time, double *scale_factor)
+void gameUpdate(Uint64 current_time, SDL_Rect *rects, const size_t num_rects)
 {
 	double period = 750.0; // ms
 	double amplitude = 0.15;
 	unsigned long total_complete_periods = (unsigned long) ((double) current_time / period);
 	double phase = ((double) current_time - ((double) total_complete_periods * period)) / period;
-	*scale_factor = 1.0 + amplitude * sin(2 * M_PI * phase);
+	double scale_factor = 1.0 + amplitude * sin(2 * M_PI * phase);
+	
+	for (size_t i = 0; i < num_rects; ++i) {
+		double size = 50.0 * scale_factor;
+		SDL_Rect r = {640 / 2 - size / 2 + 60 * ((int) i - 5), 480 / 2 - size / 2, size, size};
+		rects[i] = r;
+	}
 	
 	return;
 }
 
-void gameRender(SDL_Window *wind, SDL_Renderer *rend, SDL_Texture *tex_bubble, const double scale_factor)
+void gameRender(SDL_Window *wind, SDL_Renderer *rend, SDL_Texture *tex_bubble, const SDL_Rect *rects, const size_t num_rects)
 {
 	int wind_width;
 	int wind_height;
@@ -39,13 +52,12 @@ void gameRender(SDL_Window *wind, SDL_Renderer *rend, SDL_Texture *tex_bubble, c
 	SDL_RenderClear(rend);
 	
 	// draw assets
-	double size = 200.0 * scale_factor;
-	const SDL_Rect bubble_rect = {wind_width / 2 - size / 2, wind_height / 2 - size / 2, size, size};
-	SDL_RenderCopy(rend, tex_bubble, NULL, &bubble_rect);
+	for (size_t i = 0; i < num_rects; ++i) {
+		SDL_RenderCopy(rend, tex_bubble, NULL, &rects[i]);
+	}
 	
-	// draw to window
+	// render on window
 	SDL_RenderPresent(rend);
-	
 	return;
 }
 
@@ -57,20 +69,20 @@ void gameDestroy(SDL_Texture *tex_bubble)
 
 int gameRun(SDL_Window *wind, SDL_Renderer *rend)
 {
+	SDL_Rect rects[RECTS_SIZE];
+	
 	SDL_Texture *tex_bubble = 0;
-	if (gameInitialize(rend, &tex_bubble)) // returns true on error
+	if (gameInitialize(rend, &tex_bubble, rects, RECTS_SIZE)) // returns true on error
 		return -1;
 	
 	int game_running = 1;
-	double scale_factor = 1.0;
 	Uint64 simulation_time = SDL_GetTicks64();
-	
 	while (game_running) {
 		Uint64 current_time = SDL_GetTicks64();
 		
 		while (simulation_time < current_time) {
 			simulation_time += MS_PER_UPDATE;
-			gameUpdate(simulation_time, &scale_factor);
+			gameUpdate(simulation_time, rects, RECTS_SIZE);
 			SDL_Event event;
 			// input events
 			while (SDL_PollEvent(&event)) {
@@ -81,10 +93,9 @@ int gameRun(SDL_Window *wind, SDL_Renderer *rend)
 			}
 		}
 		
-		gameRender(wind, rend, tex_bubble, scale_factor);
+		gameRender(wind, rend, tex_bubble, rects, RECTS_SIZE);
 	}
 	
 	gameDestroy(tex_bubble);
-	
 	return 0;
 }
--- game.h
+++ game.h
@@ -11,9 +11,9 @@
 //extern const Uint64 MIN_MS_PER_DRAW_CALL; // not used and may never be used
 extern const Uint64 MS_PER_UPDATE;
 
-int gameInitialize(SDL_Renderer*, SDL_Texture**);
-void gameUpdate(Uint64, double*);
-void gameRender(SDL_Window*, SDL_Renderer*, SDL_Texture*, const double);
+int gameInitialize(SDL_Renderer*, SDL_Texture**, SDL_Rect*, const size_t);
+void gameUpdate(Uint64, SDL_Rect*, const size_t);
+void gameRender(SDL_Window*, SDL_Renderer*, SDL_Texture*, const SDL_Rect*, const size_t);
 void gameDestroy(SDL_Texture*);
 int gameRun(SDL_Window*, SDL_Renderer*);
 
