77820138df4d5f4983caf17d38bb603c3a4dac2c
xplusc <46425076+xplusc@users.noreply.github.com>
2024-07-05 20:28:19 -0700

    Update build.sh with new main.c name

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 gcc -o bubbles \
 	-W -Wall -Wextra -pedantic \
-	bubbles.c \
+	main.c \
 	game.c \
 	-I./SDL2/include/SDL2 -I./SDL2_image/include/SDL2 \
 	-L./SDL2/lib -L./SDL2_image/lib \
